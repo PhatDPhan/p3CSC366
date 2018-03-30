213b03e9c52b623d96962e099a5b735f33cacedc
PhatDPhan <33110554+PhatDPhan@users.noreply.github.com>
2018-03-30 02:31:38 -0400

    commit

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -4,8 +4,26 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream> 
- 
+#include <sstream>
+#include <string>
+
 using namespace std;
+ 
+ 
+ 
+ // Returns true if s is a number else false
+bool isNumber(string s)
+{
+    for (int i = 0; i < s.length(); i++)
+        if (isdigit(s[i]) == false)
+            return false;
+ 
+    return true;
+}
+ 
+ 
+ 
+ 
  
 // main function -
 // where the execution of program begins
@@ -27,15 +45,19 @@ int main()
 	string a[1000];
 	string x;
 	
+	string tableEntry[60];
+	
 	//read the file
 
 	//counter how many lines in the text file
 	int number_of_lines;
+	//
+	int number_of_words;
 	
 	//read line by line
-	while (getline(inFile,x))	
+	//while (getline(inFile,x))	
 	//read word by word (one space => next line)
-	//while (inFile >> x)
+	while (inFile >> x)
 	{
 			
 		//	print the x to screen;		
@@ -43,16 +65,57 @@ int main()
 		//std:: cout << x << endl;
 		
 		// save the damn line to array of that number of line		
-		a[number_of_lines] = x;
+		a[number_of_words] = x;
 		
 		//count number of line
-		number_of_lines++;
+		//number_of_lines++;
+		
+		number_of_words++;
 				
 	}
 	
 	//checked-worked. print line 2
-	std:: cout << a[1];
+	//std:: cout << a[2];
 	
+	
+	
+	string y;
+	for(int i=0; i<number_of_words; i++)
+	{
+		if(a[i] == "cmt")
+		{
+			cout << i << endl;
+			cout << a[i] << endl;
+			cout << "this is a comment"<< endl;			
+		}else if(a[i] == "get")
+		{
+			cout << i<< endl;
+			cout << a[i] << endl;
+			cout << "this is a read method"<< endl;
+		}else if(a[i] == "output")
+		{
+			cout << i<< endl;
+			cout << a[i] << endl;
+			cout << "this is a write method"<< endl;
+		}else if(a[i] == "goto")
+		{
+			cout << i<< endl;
+			cout << a[i] << endl;
+			cout << "this is a branch"<< endl;
+		}else if (a[i] == "stop")
+		{
+			cout << i<< endl;
+			cout << a[i] << endl;
+			cout << "this is a halt method"<< endl;
+		}else if (isNumber(a[i]))
+		{
+			//do sth
+			//cout << "move to this line number"<< endl;
+		}
+		
+		
+		
+	}
 	//print total number of line
 	//checked - worked
 	//std:: cout << number_of_lines;
